4a34fc1d190c9823a522721a2e042ed5a746d1c1
Elif <eliiftum@gmail.com>
2023-07-25 17:36:57 +0300

    ✅ Folders created

---
+++ AIImageGenerator/AIImageGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIImageGenerator
+//
+//  Created by Elif Tum on 25.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
